5358193460cbbfe8d2532c301039eb21177c8f99
Auxence VILLERET <avilleret@gaming.tech>
2026-01-19 10:02:45 +0100

    [ADD] Player movement

--- src/LibGameplay/Player.cpp
+++ src/LibGameplay/Player.cpp
@@ -10,6 +10,8 @@ Player::Player()
 	m_material.color = cpu::ToColor(255, 128, 0);
 	m_pCpuEntity->pMesh = &m_mesh;
 	m_pCpuEntity->pMaterial = &m_material;
+	m_speedRotation = 0.8f;
+	m_speedMovement = 4.0f;
 }
 
 Player::~Player()
@@ -23,9 +25,15 @@ void Player::Update(float dt)
 
 }
 
-void Player::Move(float x, float y, float z, float dt)
+void Player::Move(float dist, float dt)
 {
-	
+	m_pCpuEntity->transform.Move(dist * dt * m_speedMovement);
+}
+
+void Player::Rotate(float x, float y,float z, float dt)
+{
+	m_pCpuEntity->transform.AddYPR(x * dt * m_speedRotation, y * dt * m_speedRotation, z * dt * m_speedRotation * 1.5f);
+
 }
 
 #endif
--- src/LibGameplay/Player.h
+++ src/LibGameplay/Player.h
@@ -8,13 +8,19 @@ public:
 	Player();
 	~Player();
 	void Update(float dt);
-	void Move(float x, float y, float z, float dt);
+	void Move(float dist, float dt);
+
+	void Rotate(float x, float y,float z, float dt);
 
 private:
 	float m_health;
+	float m_speedRotation;
+	float m_speedMovement;
 
 	cpu_mesh m_mesh;
 	cpu_material m_material;
+
+
 };
 
 #endif
