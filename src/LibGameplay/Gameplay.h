9a1e5e44715491711d27b8f83fa21eb1540fd343
Auxence VILLERET <avilleret@gaming.tech>
2026-01-27 17:42:40 +0100

    [REF] methode projection

--- src/LibGameplay/Gameplay.h
+++ src/LibGameplay/Gameplay.h
@@ -42,4 +42,27 @@ inline float RandomRange(float min, float max)
 {
     return min + static_cast<float>(rand()) / RAND_MAX * (max - min);
 }
+
+inline bool WorldToScreen(XMFLOAT3 const& world, XMFLOAT2& screen, int screenWidth, int screenHeight)
+{
+    XMFLOAT4X4 viewProj = cpuEngine.GetCamera()->matViewProj;
+
+    float clip[4];
+
+    clip[0] = world.x * viewProj.m[0][0] + world.y * viewProj.m[1][0] + world.z * viewProj.m[2][0] + viewProj.m[3][0];
+    clip[1] = world.x * viewProj.m[0][1] + world.y * viewProj.m[1][1] + world.z * viewProj.m[2][1] + viewProj.m[3][1];
+    clip[2] = world.x * viewProj.m[0][2] + world.y * viewProj.m[1][2] + world.z * viewProj.m[2][2] + viewProj.m[3][2];
+    clip[3] = world.x * viewProj.m[0][3] + world.y * viewProj.m[1][3] + world.z * viewProj.m[2][3] + viewProj.m[3][3];
+
+    if (clip[3] <= 0.0f)
+        return false;
+
+    float ndcX = clip[0] / clip[3];
+    float ndcY = clip[1] / clip[3];
+
+    screen.x = (ndcX * 0.5f + 0.5f) * static_cast<float>(screenWidth);
+    screen.y = (1.0f - (ndcY * 0.5f + 0.5f)) * static_cast<float>(screenHeight);
+
+    return true;
+}
 #endif
